f28118594111c85fbfbe3a4d225ee2278afcc34e
Kevin Villalobos <kevinv15v@gmail.com>
2020-01-29 05:54:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,13 +3,15 @@
 
 def key_for_min_value(name_hash)
   lowest_number = 0
-  lowest_name = nil 
+  lowest_name = nil  
    
    name_hash.each do |name, number|
     if number < lowest_number
        lowest_number = number
        lowest_name = name
-   else lowest_name
-   end 
+     end
   end
+  
+  lowest_name
+  
 end
